eadeaefa2f366cba5e6044d4326eba94ad8688da
mr-phlames <mickdd22@gmail.com>
2019-11-18 15:34:36 +0000

    sanitized the console config

--- config/console.php
+++ config/console.php
@@ -9,26 +9,45 @@ class Console {
 	public function __construct() {
 		$this->app = new Application();
 
+		// Random Commands
+		$this->app->add(new \Config\Command\ServerCommand());
+		$this->app->add(new \Config\Command\ConsoleCommand());
+
+		// Generate Commands
 		$this->app->add(new \Config\Command\GenerateTemplateCommand());
 		$this->app->add(new \Config\Command\GenerateMigrationCommand());
 		$this->app->add(new \Config\Command\GenerateModelCommand());
 		$this->app->add(new \Config\Command\GenerateHelperCommand());
 		$this->app->add(new \Config\Command\GenerateControllerCommand());
 		$this->app->add(new \Config\Command\GenerateApiControllerCommand());
-		$this->app->add(new \Config\Command\DatabaseMigrationCommand());
+
+		// Delete Commands
 		$this->app->add(new \Config\Command\DeleteControllerCommand());
-		$this->app->add(new \Config\Command\ServerCommand());
-		$this->app->add(new \Config\Command\ConsoleCommand());
+
+		// Database Commands
+		$this->app->add(new \Config\Command\DatabaseMigrationCommand());
 		$this->app->add(new \Config\Command\DatabaseRollbackCommand());
 		$this->app->add(new \Config\Command\DatabaseSeedCommand());
+
+		// UI Commands
 		// $this->app->add(new \Config\Command\UiCommand());
 		$this->app->add(new \Config\Command\AuthCommand());
 	}
 
+	/**
+	 * Register a custom command
+	 * 
+	 * @param Symfony\Component\Console\Command\Command $command: Command to run
+	 * 
+	 * @return void
+	 */
 	public function registerCustom($command) {
 		$this->app->add($command);
 	}
 
+	/**
+	 * Run the console app
+	 */
 	public function run() {
 		$this->app->run();
 	}
