64e365230e3ba832d43c5be7d27aec7b8d77d8fe
mr-phlames <mickdd22@gmail.com>
2020-07-13 05:31:01 +0000

    :art: cleared out views directory

---
+++ app/views/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Leaf MVC</title>
+</head>
+
+<body>
+  <div>
+    <h3>Yay, you're on Leaf MVC🎉</h3>
+    <p>
+      This is the LeafMVC framework. A simple MVC framework built for <a href="https://leafphp.netlify.app" class="wynter text-green">Leaf PHP Framework</a>
+    </p>
+    <h4>Getting Started</h4>
+    <p>
+      Open up <code>app/routes/web.php</code> to add your routes.
+    </p>
+    <p>
+      Edit <code>app/views/pages/index.vein.php</code> and reload to see changes.
+    </p>
+    <p>
+      Open up the root directory in your console and type <code>php leaf list</code> to view all available commands.
+    </p>
+    <h4>Learning LeafMVC</h4>
+    <p>
+      <ul class="wynter list">
+        <li>
+          <a href="https://leafmvc.netlify.app" class="wynter text-green">Read our informative docs</a>
+        </li>
+        <li>
+          <a href="https://leafphp.netlify.app" class="wynter text-green">Read Leaf Framework's docs</a>
+        </li>
+      </ul>
+    </p>
+  </div>
+</body>
+
+</html>
