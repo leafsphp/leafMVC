d0ff77e519260d683a63345b78e5833875e16878
darko-mychi <mickdd22@gmail.com>
2021-01-08 19:06:03 +0000

    :sparkles: Tidy auth config

--- Config/auth.php
+++ Config/auth.php
@@ -3,45 +3,150 @@
 use Leaf\Helpers\Password;
 
 return [
-    // automatically add created_at/updated_at timestamps?
+    /*
+    |--------------------------------------------------------------------------
+    | Generate timestamps
+    |--------------------------------------------------------------------------
+    |
+    | Automatically generate created_at/updated_at timestamps for register
+    | and update methods
+    |
+    */
     "USE_TIMESTAMPS" => true,
-    // Password encode is run when leaf wants to encode passwords on register
-    // This exact method is used by default in Leaf, so you can delete it if
-    // you want to.
+
+    /*
+    |--------------------------------------------------------------------------
+    | Encode password
+    |--------------------------------------------------------------------------
+    |
+    | Password encode is run when leaf wants to encode passwords on register
+    | This exact method is used by default in Leaf, so you can delete it if
+    | you want to.
+    |
+    */
     "PASSWORD_ENCODE" => function ($password) {
         return Password::hash($password);
     },
-    // this function is run to verify the password. It's done by default,
-    // so you can remove/set this and the above lines null if you wish to.
+
+    /*
+    |--------------------------------------------------------------------------
+    | Verify Password
+    |--------------------------------------------------------------------------
+    |
+    | this function is run to verify the password. It's done by default,
+    | so you can remove/set this and the above lines null if you wish to.
+    |
+    */
     "PASSWORD_VERIFY" => function ($password, $hashedPassword) {
         // Inside the password_verify method, you have access to the password and the hashed password
         return Password::verify($password, $hashedPassword);
     },
-    // the default password key
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Key
+    |--------------------------------------------------------------------------
+    |
+    | the default password key
+    |
+    */
     "PASSWORD_KEY" => "password",
-    // Hide id field from user object?
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hide id
+    |--------------------------------------------------------------------------
+    |
+    | Hide id field from user object?
+    |
+    */
     "HIDE_ID" => true,
-    // Hide password field from user object?
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hide password
+    |--------------------------------------------------------------------------
+    |
+    | Hide user password field
+    |
+    */
     "HIDE_PASSWORD" => true,
-    // Login params error
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login params error
+    |
+    */
     "LOGIN_PARAMS_ERROR" => "Username not registered!",
-    // Login password error
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login password error
+    |
+    */
     "LOGIN_PASSWORD_ERROR" => "Password is incorrect!",
-    // USE_SESSION is experimental!!!
-    // If you encounter any problems using any new auth session features,
-    // revert to the default auth and manage sessions manually.
-    // Don't forget to open an issue.
+
+    /*
+    |--------------------------------------------------------------------------
+    | Use session [EXPERIMENTAL]
+    |--------------------------------------------------------------------------
+    |
+    | Use session based authentication instead of the default JWT based auth.
+    |
+    | If you encounter any problems using any new auth session features,
+    | revert to the default auth and manage sessions manually.
+    | Don't forget to open an issue.
+    |
+    */
     "USE_SESSION" => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Template Engine [EXPERIMENTAL]
+    |--------------------------------------------------------------------------
+    |
+    | Leaf MVC unlike other frameworks tries to give you as much control as
+    | you need. As such, you can decide which view engine to use.
+    |
+    */
     // Create a session on registration?
     "SESSION_ON_REGISTER" => false,
-    // Login page
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login page route
+    |
+    */
     "GUARD_LOGIN" => "/auth/login",
-    // Register page
+
+    /*
+    |--------------------------------------------------------------------------
+    | Rehgister page route
+    |
+    */
     "GUARD_REGISTER" => "/auth/register",
-    // Logout route
+
+    /*
+    |--------------------------------------------------------------------------
+    | Logout route
+    |
+    */
     "GUARD_LOGOUT" => "/auth/logout",
-    // Home page
+
+    /*
+    |--------------------------------------------------------------------------
+    | Home page route
+    |
+    */
     "GUARD_HOME" => "/home",
-    // Add an auth token to the auth session?
+
+    /*
+    |--------------------------------------------------------------------------
+    | JWT + Session
+    |--------------------------------------------------------------------------
+    |
+    | Add an auth token to the auth session?
+    |
+    */
     "SAVE_SESSION_JWT" => false,
 ];
--- Config/paths.php
+++ Config/paths.php
@@ -2,16 +2,28 @@
 
 return [
 	"controllers_path" => "App/Controllers",
+
 	"models_path" => "App/Models",
+
 	"migrations_path" => "App/Database/Migrations",
+
 	"seeds_path" => "App/Database/Seeds",
+
 	"factories_path" => "App/Database/Factories",
+
 	"helpers_path" => "App/Helpers",
+
 	"views_path" => "App/Views",
+
 	"config_path" => "App/Config",
+
 	"storage_path" => "App/storage",
+
 	"commands_path" => "App/Console",
+
 	"routes_path" => "App/Routes",
+
 	"lib_path" => "Lib",
+
 	"public_path" => "public",
 ];
