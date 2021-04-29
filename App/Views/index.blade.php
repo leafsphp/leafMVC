5eb982ec32ef4e4b2f16a6815593c4d21af9e039
mychi darko <mickdd22@gmail.com>
2021-04-29 11:57:57 +0000

    :sparkles: added new leafmvc link

--- App/Views/index.blade.php
+++ App/Views/index.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>{{ getenv('APP_NAME') ?? "Leaf MVC" }}</title>
   <link rel="shortcut icon" href="https://leafphp.netlify.app/img/logo.png" type="image/x-icon">
-  <link rel="stylesheet" href="{{ views_path("assets/css/styles.css", true) }}">
+  <link rel="stylesheet" href="{{ views_path("assets/css/styles.css") }}">
 </head>
 
 <body>
@@ -14,25 +14,37 @@
     <section>
       <h1>{{ getenv('APP_NAME') ?? "Leaf MVC" }}</h1>
       <p>
-        This is the LeafMVC framework. A simple MVC framework built for <a href="https://leafphp.netlify.app" class="wynter text-green">Leaf PHP Framework</a>
+        This is the LeafMVC framework. A simple MVC framework built for
+        <a href="https://leafphp.netlify.app" class="wynter text-green">Leaf PHP Framework</a>
       </p>
     </section>
     <section>
       <h2>Getting Started</h2>
       <ul>
-        <li>Open up <code>App/Routes/index.php</code> to add your routes.</li>
-        <li>Edit <code>App/Views/index.blade.php</code> and reload to see changes.</li>
-        <li>Open up the root directory in your console and type <code>php leaf list</code> to view all available commands.</li>
+        <li>Open up <b><code>App/Routes/index.php</code></b> to add your routes.</li>
+        <li>Edit <b><code>App/Views/index.blade.php</code></b> and reload to see changes.</li>
+        <li>
+            Open up the root directory in your console and type <code>php leaf list</code>
+            to view all available commands.
+        </li>
         <li>Grow something amazing. Happy gardening!</li>
       </ul>
     </section>
     <section>
-      <h2>Learning Leaf MVC</h2>
-      <div class="collection">
-        <a href="https://leafmvc.netlify.app" target="_blank" class="collection-item">Read our informative docs</a>
-        <a href="https://leafphp.netlify.app" target="_blank" class="collection-item">Read Leaf Framework's docs</a>
-        <a href="https://twitter.com/leafphp" target="_blank" class="collection-item">Follow @leafphp on twitter for updates on Leaf</a>
-        <a href="https://www.youtube.com/channel/UCllE-GsYy10RkxBUK0HIffw" target="_blank" class="collection-item">Subscribe to our Youtube channel</a>
+      <button @click="showExtras = !showExtras">Learn Leaf MVC</button>
+      <div class="collection" v-if="showExtras">
+        <a href="https://leafphp.netlify.app/#/leaf-mvc/" target="_blank" class="collection-item">
+            Read our informative docs
+        </a>
+        <a href="https://leafphp.netlify.app/#/leaf/" target="_blank" class="collection-item">
+            Read Leaf Framework's docs
+        </a>
+        <a href="https://twitter.com/leafphp" target="_blank" class="collection-item">
+            Follow @leafphp on twitter for updates on Leaf
+        </a>
+        <a href="https://www.youtube.com/channel/UCllE-GsYy10RkxBUK0HIffw" target="_blank" class="collection-item">
+            Subscribe to our Youtube channel
+        </a>
       </div>
     </section>
   </div>
