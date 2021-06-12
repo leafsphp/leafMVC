f327e407c3508e8a8d30c277ca63d7cf64c1d541
Mychi Darko <mickdd22@gmail.com>
2021-06-12 15:16:31 +0000

    :alien: added database config

---
+++ Config/database.php
@@ -0,0 +1,91 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Database Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the database connections below you wish
+    | to use as your default connection for all database work. Of course
+    | you may use many connections at once using the Database library.
+    |
+    */
+
+    "default" => _env("DB_CONNECTION", "mysql"),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the database connections setup for your application.
+    | Of course, examples of configuring each database platform that is
+    | supported by eloquent is shown below to make development simple.
+    |
+    |
+    | All database work in eloquent is done through the PHP PDO facilities
+    | so make sure you have the driver for your particular database of
+    | choice installed on your machine before you begin development.
+    |
+    */
+
+    "connections" => [
+        "sqlite" => [
+            "driver" => "sqlite",
+            "url" => _env("DATABASE_URL"),
+            "database" => _env("DB_DATABASE", database_path("database.sqlite")),
+            "prefix" => "",
+            "foreign_key_constraints" => _env("DB_FOREIGN_KEYS", true),
+        ],
+
+        "mysql" => [
+            "driver" => "mysql",
+            "url" => _env("DATABASE_URL"),
+            "host" => _env("DB_HOST", "127.0.0.1"),
+            "port" => _env("DB_PORT", "3306"),
+            "database" => _env("DB_DATABASE", "forge"),
+            "username" => _env("DB_USERNAME", "forge"),
+            "password" => _env("DB_PASSWORD", ""),
+            "unix_socket" => _env("DB_SOCKET", ""),
+            "charset" => _env("DB_CHARSET", "utf8mb4"),
+            "collation" => _env("DB_COLLATION", "utf8mb4_unicode_ci"),
+            "prefix" => "",
+            "prefix_indexes" => true,
+            "strict" => true,
+            "engine" => null,
+            "options" => extension_loaded("pdo_mysql") ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => _env("MYSQL_ATTR_SSL_CA"),
+            ]) : [],
+        ],
+
+        "pgsql" => [
+            "driver" => "pgsql",
+            "url" => _env("DATABASE_URL"),
+            "host" => _env("DB_HOST", "127.0.0.1"),
+            "port" => _env("DB_PORT", "5432"),
+            "database" => _env("DB_DATABASE", "forge"),
+            "username" => _env("DB_USERNAME", "forge"),
+            "password" => _env("DB_PASSWORD", ""),
+            "charset" => _env("DB_CHARSET", "utf8"),
+            "prefix" => "",
+            "prefix_indexes" => true,
+            "schema" => "public",
+            "sslmode" => "prefer",
+        ],
+
+        "sqlsrv" => [
+            "driver" => "sqlsrv",
+            "url" => _env("DATABASE_URL"),
+            "host" => _env("DB_HOST", "localhost"),
+            "port" => _env("DB_PORT", "1433"),
+            "database" => _env("DB_DATABASE", "forge"),
+            "username" => _env("DB_USERNAME", "forge"),
+            "password" => _env("DB_PASSWORD", ""),
+            "charset" => _env("DB_CHARSET", "utf8"),
+            "prefix" => "",
+            "prefix_indexes" => true,
+        ],
+    ],
+];
