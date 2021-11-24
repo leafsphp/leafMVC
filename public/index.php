0342c1047420ca7bbe733d3c62ed3a9f552cfe25
Michael Darko <mickdd22@gmail.com>
2021-11-24 17:45:55 +0000

    :art: moved project root to public

---
+++ public/index.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Switch to root path
+|--------------------------------------------------------------------------
+|
+| Point to the application root directory so leaf can accurately
+| resolve app paths.
+|
+*/
+chdir(dirname(__DIR__));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Bring in (env)
+|--------------------------------------------------------------------------
+|
+| Quickly use our environment variables
+|
+*/
+try {
+    \Dotenv\Dotenv::createUnsafeImmutable(dirname(__DIR__))->load();
+} catch (\Throwable $th) {
+    trigger_error($th);
+}
+
+/*
+|--------------------------------------------------------------------------
+| Load application paths
+|--------------------------------------------------------------------------
+|
+| Tell Leaf MVC Core where to locate application paths
+|
+*/
+Leaf\Core::paths(PathsConfig());
+
+/*
+|--------------------------------------------------------------------------
+| Load application paths
+|--------------------------------------------------------------------------
+|
+| Decline static file requests back to the PHP built-in webserver
+|
+*/
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+    if (is_string($path) && __FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+
+    unset($path);
+}
+
+/*
+|--------------------------------------------------------------------------
+| Attach blade view
+|--------------------------------------------------------------------------
+|
+| Since blade no longer ships with Leaf by default, we
+| can attach blade back to Leaf so you can use Leaf MVC
+| as you've always used it.
+|
+*/
+Leaf\View::attach(\Leaf\Blade::class);
+
+/*
+|--------------------------------------------------------------------------
+| Initialise Leaf Core
+|--------------------------------------------------------------------------
+|
+| Plant a seed, grow the stem and return Leaf🤷‍
+|
+*/
+Leaf\Config::set(AppConfig());
+
+/*
+|--------------------------------------------------------------------------
+| Default fix for CORS
+|--------------------------------------------------------------------------
+|
+| This just prevents the connection client from throwing
+| CORS errors at you.
+|
+*/
+app()->cors(CorsConfig());
+
+/*
+|--------------------------------------------------------------------------
+| Additional Leaf Database Config
+|--------------------------------------------------------------------------
+|
+| Load leaf database configuration
+|
+*/
+Leaf\Database::config(DatabaseConfig());
+
+/*
+|--------------------------------------------------------------------------
+| Route Config
+|--------------------------------------------------------------------------
+|
+| Require app routes.
+|
+*/
+require dirname(__DIR__) . "/app/routes/index.php";
+
+/*
+|--------------------------------------------------------------------------
+| Run Leaf Application
+|--------------------------------------------------------------------------
+|
+| Require app routes
+|
+*/
+app()->run();
