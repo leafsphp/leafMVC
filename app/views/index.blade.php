5ec9f7e4b567e1e0189a3d43e5885ca4d5297ae9
Michael Darko <mickdd22@gmail.com>
2022-02-17 23:41:02 +0000

    :art: updated styles

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ getenv('APP_NAME') ?? 'Leaf MVC' }}</title>
-    <link rel="shortcut icon" href="https://leafphp.netlify.app/img/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="https://leafphp.dev/logo-circle.png" type="image/x-icon">
     <link rel="stylesheet" href="{{ PublicPath('assets/css/styles.css') }}">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700;display=swap">
