75fd9019ca4b1f3ec985c0fc0a5b2c071937c7af
Michael Darko <mickdd22@gmail.com>
2022-02-17 23:41:26 +0000

    :bulb: updated comments

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -2,9 +2,6 @@
 
 namespace App\Controllers;
 
-// Leaf Auth is a package which makes user authentication simple
-use Leaf\Auth;
-
 /**
  * This is the base controller for your Leaf MVC Project.
  * You can initialize packages or define methods here to use
@@ -17,19 +14,19 @@ class Controller extends \Leaf\Controller
         parent::__construct();
 
         // In this version, request isn't initialised for you. You can use
-        // requestData() or request() to get request data or initialise it yourself
+        // request() to get request data or initialise it yourself
 
         // autoConnect uses the .env variables to quickly connect to db
-        Auth::autoConnect();
+        auth()->autoConnect();
 
         // You can configure auth to get additional customizations
-        // This can be done here with the Auth::config method or
+        // This can be done here with the auth()->config method or
         // simply in the config/auth.php file
-        Auth::config(AuthConfig());
+        auth()->config(AuthConfig());
 
         // You can refer to https://leafphp.dev/modules/auth for auth docs
 
-        // To use session instead of JWT, open up config/auth.php and set
-        // USE_SESSION to true
+        // To use JWT instead of sessions, open up config/auth.php and set
+        // USE_SESSION to false
     }
 }
