5484d6c9a202ce5a7bfdfa649aa559354d3e6f9f
Michael Darko <mickdd22@gmail.com>
2022-05-29 23:23:48 +0000

    🧑‍💻 simplified db connection

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -17,7 +17,8 @@ class Controller extends \Leaf\Controller
         // request() to get request data or initialise it yourself
 
         // autoConnect uses the .env variables to quickly connect to db
-        auth()->autoConnect();
+        // Leaf auth will smartly connect to this db connection
+        db()->autoConnect();
 
         // You can configure auth to get additional customizations
         // This can be done here with the auth()->config method or
