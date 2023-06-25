89f4ffac7be87868ad6f3186594f7e86818b34ec
mychidarko <mickdd22@gmail.com>
2023-06-25 13:04:41 +0000

    fix: update leaf db in example factory

--- app/database/factories/UserFactory.php
+++ app/database/factories/UserFactory.php
@@ -6,22 +6,22 @@ use App\Models\User;
 
 class UserFactory extends Factory
 {
-	// If this model property isn't defined, Leaf will
-	// try to generate the model name from the factory name
-	public $model = User::class;
+    // If this model property isn't defined, Leaf will
+    // try to generate the model name from the factory name
+    public $model = User::class;
 
-	// You define your factory blueprint here
-	// It should return an associative array
-	public function definition(): array
-	{
-		return [
-			'username' => strtolower($this->faker->firstName),
-			'fullname' => $this->faker->name,
-			'email' => $this->faker->unique()->safeEmail,
-			'email_verified_at' => \Leaf\Date::now(),
-			'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-			// $this->str is defined in the base factory
-			'remember_token' => $this->str::random(10),
-		];
-	}
+    // You define your factory blueprint here
+    // It should return an associative array
+    public function definition(): array
+    {
+        return [
+            'username' => strtolower($this->faker->firstName),
+            'fullname' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => tick()->now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // $this->str is defined in the base factory
+            'remember_token' => $this->str::random(10),
+        ];
+    }
 }
