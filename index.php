cddba7a3f262e91433170cc6d0bd4254d06595b3
mychidarko <mickdd22@gmail.com>
2023-08-08 23:47:13 +0000

    feat: update index file

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php
 
-/**
- * Leaf MVC - A minimal but powerful PHP MVC framework
- *
- * @package  Leaf MVC
- * @author   Michael Darko <mychi.darko@gmail.com>
- */
+/*
+|--------------------------------------------------------------------------
+| Leaf MVC
+|--------------------------------------------------------------------------
+|
+| Leaf MVC is a minimal but powerful PHP MVC framework based on
+| the Leaf PHP framework.
+|
+| This file allows us to run the app from the root of the project.
+| This provides a convenient way to test your Leaf MVC app
+| without having installed a "real" web server software here.
+|
+| It also allows you to directly load up your application from
+| the root file for quickly hosting on shared hosting platforms.
+|
+*/
 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-// This file allows us to run the app from the root of the project.
-// This provides a convenient way to test your Leaf MVC app
-// without having installed a "real" web server software here.
-
-// It also allows you to directly load up your application from
-// the root file for quickly hosting on shared hosting platforms.
 if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
     return false;
 }
