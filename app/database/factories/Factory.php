b97759e15219db5fd7f27cbfdfa1dafb6620f53d
mychidarko <mickdd22@gmail.com>
2023-08-08 09:49:54 +0000

    feat: update base factory

--- app/database/factories/Factory.php
+++ app/database/factories/Factory.php
@@ -3,26 +3,19 @@
 namespace App\Database\Factories;
 
 use Leaf\Factory as Base;
-use Illuminate\Support\Str;
 
 /**
  * Base Factory Class
  * ----------------
  * You can define methods here that would be used
  * throughout your factory classes.
+ *
+ * This is a good place to initialize
+ * anything you want to use in your factories
+ *
+ * We have already initialized faker and the Str for you
+ * which you can use on $this->faker and $this->str respectively
  */
 class Factory extends Base
 {
-    public $str;
-
-    /**@var \Faker\Generator $faker*/
-    public $faker;
-
-    public function __construct()
-    {
-        // Just for demo purposes, str is assigned here
-        // so you can use it in all your factories.
-        $this->str = Str::class;
-        $this->faker = \Faker\Factory::create();
-    }
 }
