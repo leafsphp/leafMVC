28ee8e93737b3de8cd92ec7cebcfa69bed0e5b8c
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-08-19 23:03:41 +0300

    feat: csrf file config

--- config/csrf.php
+++ config/csrf.php
@@ -1,1 +1,23 @@
 
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| CSRF Configuration
+|--------------------------------------------------------------------------
+|
+| This file contains the configuration for CSRF protection.
+| CSRF protection is important in preventing
+| Cross Site Request Forgery attacks.
+|
+*/
+
+return [
+    'SECRET_KEY' => '_token',
+    'SECRET' => getenv('APP_KEY'),
+    'METHODS' => ['POST', 'PUT', 'PATCH', 'DELETE'],
+    'EXCEPT' => [
+        '/api/{wild}',
+        '/hook/{wild}'
+    ]
+];
