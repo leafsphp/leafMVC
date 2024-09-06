6565df975f5540e1975e112ab3a6c8ea3281f67e
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-09-06 11:42:18 +0300

    Commands Registrar

--- app/console/Commands.php
+++ app/console/Commands.php
@@ -11,7 +11,7 @@ class Commands
      * @return void
      * 
      */
-    public static function register($console, $scan=true): void
+    public static function register($console): void
     {
         $console->register([
             ExampleCommand::class,
